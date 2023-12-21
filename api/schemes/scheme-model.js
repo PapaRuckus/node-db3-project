3a1935b4826b53143ba29102604e169175192619
Calvin K <calvinknaub@gmail.com>
2023-12-21 14:40:52 -0800

    finsihed findById

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -5,7 +5,7 @@ function find() {
   return db("schemes as sc")
     .leftJoin("steps as st", "sc.scheme_id", "st.scheme_id")
     .select("sc.*")
-    .count('st.step_id as number_of_steps')
+    .count("st.step_id as number_of_steps")
     .groupBy("sc.scheme_id");
   /*
     1A- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`.
@@ -25,7 +25,30 @@ function find() {
   */
 }
 
-function findById(scheme_id) {
+async function findById(scheme_id) {
+  const rows = await db("schemes as sc")
+    .leftJoin("steps as st", "sc.scheme_id", "st.scheme_id")
+    .where("sc.scheme_id", scheme_id)
+    .select("st.*", "sc.scheme_name", 'sc.scheme_id')
+    .orderBy("st.step_number"); 
+  
+  const result = {
+    scheme_id: rows[0].scheme_id,
+    scheme_name: rows[0].scheme_name,
+    steps: []
+  };
+
+  rows.forEach(row => {
+    if (row.step_id) {
+      result.steps.push({
+        step_id: row.scheme_id,
+        step_number: row.step_number,
+        instructions: row.instructions
+      });
+    }
+  })
+  return result
+
   // EXERCISE B
   /*
     1B- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`:
